df0a4ad3cda4d8c1475039af5ae7c04d87e1bca5
batyril <237x237@gmail.ru>
2022-10-31 01:49:01 +0300

    added prettier

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,18 +1,20 @@
 const TIMER = {
-  DAYS: document.querySelector('#days'),
-  HOURS: document.querySelector('#hours'),
-  MINUTES: document.querySelector('#minutes'),
-  SECONDS: document.querySelector('#seconds'),
+  DAYS: document.querySelector("#days"),
+  HOURS: document.querySelector("#hours"),
+  MINUTES: document.querySelector("#minutes"),
+  SECONDS: document.querySelector("#seconds"),
 };
 
-const deadline = '2022-10-22T17:53';
+const deadline = "2022-10-22T17:53";
 
 function getTimeRemaining(endTime = deadline) {
   const convertTime = Date.parse(endTime);
   const nowTime = Date.parse(new Date());
   const difference = convertTime - nowTime;
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((difference % (1000 * 60)) / 1000);
   return {
@@ -27,10 +29,10 @@ function getTimeRemaining(endTime = deadline) {
 function setClock(endTime = deadline) {
   const date = getTimeRemaining(endTime);
   if (date.difference < 0) {
-    TIMER.DAYS.textContent = '00';
-    TIMER.HOURS.textContent = '00';
-    TIMER.MINUTES.textContent = '00';
-    TIMER.SECONDS.textContent = '00';
+    TIMER.DAYS.textContent = "00";
+    TIMER.HOURS.textContent = "00";
+    TIMER.MINUTES.textContent = "00";
+    TIMER.SECONDS.textContent = "00";
     return;
   }
 
@@ -39,6 +41,7 @@ function setClock(endTime = deadline) {
       date[keys] = `0${date[keys]}`;
     }
   }
+
   TIMER.DAYS.textContent = date.days;
   TIMER.HOURS.textContent = date.hours;
   TIMER.MINUTES.textContent = date.minutes;
